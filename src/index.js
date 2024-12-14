3383b43aa6fdae5fa5a3509f6779c37777f012ff
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2024-12-14 16:24:28 +0300

    edited js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const form = document.getElementById('create-task-form');
+const taskList = document.getElementById('task-list');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = document.getElementById('new-task-description').value;
+  const task = document.createElement('li');
+  task.textContent = taskDescription;
+  taskList.appendChild(task);
+  document.getElementById('new-task-description').value = '';
+});   
